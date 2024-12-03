83230e512090ab801be85653a3e014df63f24220
Věra Juračáková <juracakova.vera@gmail.com>
2024-12-03 20:09:48 +0100

    Opravení chyb :)

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
-const rateCzkEur = 24.47
-const rateCzkGbp = 28.09
-const rateCzkUsd = 24.81
-const rateCzkBtc = 478637
+const rateCZkEur = 24.47;
+const rateCZkGbp = 28.09;
+const rateCZkUsd = 24.81;
+const rateCZkBtc = 478637;
 
 const convertToCzk = (amount, currency) => {
-    if (currency.toUpperCase() === "EUR") {
-        return Math.round(amount * rateCzkEur)
-    } else if (currency.toUpperCase() === "GBP") {
-        return Math.round(amount*rateCzkGbp)
-    } else if (currency.toUpperCase() === "USD") {
-        return Math.round(amount*rateCzkUsd)
-    } else if (currency.toUpperCase() === "BTC") {
-        return Math.round(amount*rateCzkBtc)
-    } else return null
-}
+  if (currency.toUpperCase() === "EUR") {
+    return Math.round(amount * rateCZkEur);
+  } else if (currency.toUpperCase() === "GBP") {
+    return Math.round(amount * rateCZkGbp);
+  } else if (currency.toUpperCase() === "USD") {
+    return Math.round(amount * rateCZkUsd);
+  } else if (currency.toUpperCase() === "BTC") {
+    return Math.round(amount * rateCZkBtc);
+  } else return null;
+};
 
-document.body.innerHTML += "<h2>Převod z eura do CZK</h2>"
-document.body.innerHTML += `${convertToCzk(268, "EUR")} CZK`
 
-document.body.innerHTML += "<h2>Převod z Britské libry do CZK</h2>"
-document.body.innerHTML += `${convertToCzk(268, "GBP")} CZK`
+document.body.innerHTML += `<h2>Převod z eura do CZK</h2>`;
+document.body.innerHTML += `${convertToCzk(268, "EUR")} CZK`;
 
-document.body.innerHTML += "<h2>Převod z Amerického dolaru do CZK</h2>"
-document.body.innerHTML += `${convertToCzk(268, "USD")} CZK`
+document.body.innerHTML += `<h2>Převod z Britské libry do CZK</h2>`;
+document.body.innerHTML += `${convertToCzk(268, "GBP")} CZK`;
 
-document.body.innerHTML += "<h2>Převod z Bitcoinu do CZK</h2>"
-document.body.innerHTML += `${convertToCzk(268, "BTC")} CZK`
+document.body.innerHTML += `<h2>Převod z Amerického dolaru do CZK</h2>`;
+document.body.innerHTML += `${convertToCzk(268, "USD")} CZK`;
+
+document.body.innerHTML += "<h2>Převod z Bitcoinu do CZK</h2>";
+document.body.innerHTML += `${convertToCzk(268, "BTC")} CZK`;
